62133d4122769d55da969f1b4e6122a756cce332
Varun Singh <varun.singh@homeshop18.com>
2020-06-23 23:09:21 +0530

    make new function component for searc

--- app/modules/screens/Kitchen/ZipcodeSearchbar.js
+++ app/modules/screens/Kitchen/ZipcodeSearchbar.js
@@ -1,85 +1,69 @@
 import React, { useState, Fragment, Component } from 'react'
-import { Text, View,TextInput } from 'react-native'
+import { Text, View, TextInput } from 'react-native'
 import { Dropdown } from 'react-native-material-dropdown';
 import { Button } from 'react-native-paper';
 import styles from '../../styles/index';
 
 
-class ZipcodeSearchbar extends Component {
-    // setSelectedValue] = useState(1);
-    constructor(props) {
-        super(props)
-       // this.nameRef = this.updateRef.bind(this, 'name');
+const ZipcodeSearchbar = () => {
+    let [defaultZipcode, setZipCode] = useState('');
+    let [defaultState, setDefaultState] = useState('California');
+    let [defaultStateList, setDefaultStateList] = useState([
+        { value: 'California' },
+        { value: 'Illinois' },
+    ]);
 
-        this.state = {
-            name: 'California',
-            colorNameData: [
-                { value: 'California' },
-                { value: 'Illinois' },
-            ]
-        };
+    const onChangeText = (text) => {
+        setDefaultState(text)
 
     }
-    onChangeText = (text) => {
-        ['name']
-            .map((name) => ({ name, ref: this[name] }))
-            .filter(({ ref }) => ref && ref.isFocused())
-            .forEach(({ name, ref }) => {
-                this.setState({ [name]: text });
-            });
+
+
+    const handleSearch = () => {
+        console.log("handleSearch",defaultZipcode , defaultState)
     }
 
-    // updateRef(name, ref) {
-    //     this[name] = ref;
-    // }
+    return (
+        <Fragment>
 
-    
+            <View style={styles.containerSearch}>
+                <Text style={styles.textColor}>Delivery Zip Code & Kitchens</Text>
 
-    render() {
-        let { name } = this.state;
+                <View style={{ flex: 1, flexDirection: 'row' }}>
+                    <View style={{ width: 100, marginTop: -12 }}>
+                        <Dropdown
+                            value={defaultState}
+                            onChangeText={val => onChangeText(val)}
+                            label='Refine By State'
+                            data={defaultStateList}
+                            textColor="#000000"
+                            itemColor="#000000"
+                            baseColor="#574d6b"
+                            selectedItemColor="#ffa300"
+                        />
 
-        return (
-            <Fragment>
-
-                <View style={styles.containerSearch}>
-                    <Text style={styles.textColor}>Delivery Zip Code & Kitchens</Text>
-
-                    <View style={{ flex: 1, flexDirection: 'row' }}>
-                        <View style={{ width: 100 ,marginTop:-12}}>
-                            <Dropdown
-                               // ref={this.nameRef}
-                                value={this.state.name}
-                                onChangeText={this.onChangeText}
-                                label='Refine By State'
-                                data={this.state.colorNameData}
-                                textColor="#000000" 
-                                itemColor="#000000"
-                                baseColor="#574d6b"
-                                selectedItemColor="#ffa300"
-                            />
-
-                        </View>
-                        <View>
-                            <TextInput
-                                style={styles.searchTextInput}
-                                placeholder="Search by Zip code"
-                                placeholderTextColor="#574d6b" 
-                                autoCapitalize="none"
-                                secureTextEntry={true}
-                                onChangeText={val => setSearchText(val)}
-                            />
-                        </View>
-                        <View>
-                            <Button style={styles.defaultButton} onPress={val => handleSearch(val)}>
-                                + ZipCode
-                            </Button>
-                        </View>
                     </View>
-
+                    <View>
+                        <TextInput
+                            style={styles.searchTextInput}
+                            placeholder="Search by Zip code"
+                            placeholderTextColor="#574d6b"
+                            autoCapitalize="none"
+                            secureTextEntry={true}
+                            onChangeText={val => setZipCode(val)}
+                        />
+                    </View>
+                    <View>
+                        <Button style={styles.defaultButton} onPress={val => handleSearch(val)}>
+                            + ZipCode
+                            </Button>
+                    </View>
                 </View>
-            </Fragment>
-        )
-    }
+
+            </View>
+        </Fragment>
+    )
+
 
 }
 export default ZipcodeSearchbar
