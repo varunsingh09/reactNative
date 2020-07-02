58a1efda073878075afdfdb3224293c2b14f595a
Varun Singh <varun.singh@homeshop18.com>
2020-07-02 21:37:05 +0530

    add scroll pagination

--- app/modules/screens/Product/KitchenList.js
+++ app/modules/screens/Product/KitchenList.js
@@ -1,43 +1,73 @@
 import React, { useState } from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, Alert,ScrollView } from 'react-native';
+import { StyleSheet, View, Text, TouchableOpacity, Alert, ScrollView, ActivityIndicator } from 'react-native';
 import { Table, TableWrapper, Row, Cell } from 'react-native-table-component';
 
 const KitchenList = () => {
 
-    let [tableHead, setTableHead] = useState(['Head', 'Head2', 'Head3', 'Head4', 'Head5']);
-    let [widthArr, setTableData] = useState([40, 60, 80, 100, 120]);
+    let [tableHead, setTableHead] = useState(['Head1', 'Head2', 'Head3', 'Head4']);
+    let [loading, setIsLoading] = useState(false);
+    let [widthArr, setWidthArr] = useState([70, 70, 70, 100]);
+    let [tableData, setTableData] = useState([
+        ['1', '2', '3', '4',],
+        ['a', 'b', 'c', 'd',],
+        ['1', '2', '3', '4',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',],
+        ['1', '2', '3', '4',],
+        ['a', 'b', 'c', 'd',],
+        ['1', '2', '3', '4',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',],
+        ['1', '2', '3', '4',],
+        ['a', 'b', 'c', 'd',],
+        ['1', '2', '3', '4',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',],
+        ['a', 'b', 'c', 'd',]
+    ])
 
     const alertIndex = (index) => {
         Alert.alert(`This is row ${index + 1}`);
     }
 
-    const tableData = [];
-    for (let i = 0; i < 30; i += 1) {
-        const rowData = [];
-        for (let j = 0; j < 9; j += 1) {
-            rowData.push(`${i}${j}`);
-        }
-        tableData.push(rowData);
+    const onMomentumScrollEnd = (event) => {
+        setIsLoading(true)
+        console.log("onMomentumScrollEnd reach")
     }
 
+    const element = (data, index) => (
+        <TouchableOpacity onPress={() => alertIndex(index)}>
+            <View style={styles.btn}>
+                <Text style={styles.btnText}>button</Text>
+            </View>
+        </TouchableOpacity>
+    );
     return (
         <View style={styles.container}>
+            {loading === true && <ActivityIndicator size="large" />}
             <ScrollView horizontal={true}>
                 <View>
                     <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
-                        <Row data={tableHead} widthArr={widthArr} style={styles.header} textStyle={styles.text} />
+                        <Row data={tableHead}
+                            widthArr={widthArr}
+                            style={styles.header} textStyle={styles.text} />
                     </Table>
-                    <ScrollView style={styles.dataWrapper}>
+                    <ScrollView style={styles.dataWrapper} onMomentumScrollEnd={(event) => onMomentumScrollEnd(event)}>
                         <Table borderStyle={{ borderWidth: 1, borderColor: '#C1C0B9' }}>
                             {
                                 tableData.map((rowData, index) => (
-                                    <Row
-                                        key={index}
-                                        data={rowData}
-                                        widthArr={widthArr}
-                                        style={[styles.row, index % 2 && { backgroundColor: '#F7F6E7' }]}
-                                        textStyle={styles.text}
-                                    />
+                                    <TableWrapper key={index} style={styles.row}>
+                                        {
+                                            rowData.map((cellData, cellIndex) => (
+                                                <Cell key={cellIndex} data={cellIndex === 3 ? element(cellData, index) : cellData} textStyle={styles.text} />
+                                            ))
+                                        }
+                                    </TableWrapper>
                                 ))
                             }
                         </Table>
@@ -53,7 +83,7 @@ const styles = StyleSheet.create({
     header: { height: 50, backgroundColor: '#537791' },
     text: { textAlign: 'center', fontWeight: '100' },
     dataWrapper: { marginTop: -1 },
-    row: { height: 40, backgroundColor: '#E7E6E1' }
+    row: { flexDirection: 'row', backgroundColor: '#FFF1C1' },
 });
 
 export default KitchenList
